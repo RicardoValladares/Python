c4e171e628ebd47a5ae06f8121797cfeaf5ed816
RicardoValladares <r_a_v_r_@hotmail.com>
2022-11-13 19:04:48 -0600

    se agregaron comentarios https y gunicorn

--- Flask/main.py
+++ Flask/main.py
@@ -42,4 +42,17 @@ def ServicioConToken():
 
 
 if __name__ == "__main__":
+    #SERVIDOR CON HTTP
     app.run(debug=True, host='0.0.0.0', port=5002)
+    #SERVIDOR CON HTTPS
+    #app.run(host='0.0.0.0', port=5002, ssl_context=('/etc/letsencrypt/live/apps.localhost.com-0001/fullchain.pem','/etc/letsencrypt/live/apps.localhost.com-0001/privkey.pem'))
+    
+
+
+
+#EJECUCION NORMAL CON PYTHON
+#python main.py
+#EJECUCION EN PROCEOS PARALELO
+#gunicorn -w 4 -b 0.0.0.0:5002 --log-level=debug main:app --daemon
+#EJECUCION EN PROCEOS PARALELO CON CERTIFICADO HTTPS
+#gunicorn -w 4 -b 0.0.0.0:5002 --certfile=/etc/letsencrypt/live/apps.localhost.com-0001/fullchain.pem --keyfile=/etc/letsencrypt/live/apps.localhost.com-0001/privkey.pem --log-level=debug main:app --daemon
