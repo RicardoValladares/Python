341e1a20f9bef14f4518b8146f902c621449ef65
RicardoValladares <r_a_v_r_@hotmail.com>
2022-11-29 20:02:57 -0600

    mysql + ApiREST

--- Flask/ApiRest/main.py
+++ Flask/ApiRest/main.py
@@ -51,3 +51,24 @@ if __name__ == "__main__":
 #gunicorn -w 4 -b 0.0.0.0:5002 --log-level=debug main:app --daemon
 #EJECUCION EN PROCEOS PARALELO CON CERTIFICADO HTTPS
 #gunicorn -w 4 -b 0.0.0.0:5002 --certfile=/etc/letsencrypt/live/apps.localhost.com-0001/fullchain.pem --keyfile=/etc/letsencrypt/live/apps.localhost.com-0001/privkey.pem --log-level=debug main:app --daemon
+    
+
+
+
+#EJECUCION NORMAL CON PYTHON
+#python main.py
+#EJECUCION EN PROCEOS PARALELO
+#gunicorn -w 4 -b 0.0.0.0:5002 --log-level=debug main:app --daemon
+#EJECUCION EN PROCEOS PARALELO CON CERTIFICADO HTTPS
+#gunicorn -w 4 -b 0.0.0.0:5002 --certfile=/etc/letsencrypt/live/apps.localhost.com-0001/fullchain.pem --keyfile=/etc/letsencrypt/live/apps.localhost.com-0001/privkey.pem --log-level=debug main:app --daemon
+
+
+
+#response = requests.get(...)
+#json_data = json.loads(response.text)
+#import json
+#jsonStr = '{ "Documentos": [{"Tipo":"DUI", "Numero":"04655777-8"}, {"Tipo":"Pasaporte", "Numero":"B04655777"}]}'
+#aList = json.loads(jsonStr)
+#for i in aList['Documentos']:
+#	print(i['Tipo'])
+#
--- Tkinter/mysql.py
+++ Tkinter/mysql.py
@@ -1,41 +1,54 @@
 from tkinter import *
+from tkinter import ttk
 
-mi_Entrys= []
-
-lst = [('Ricardo',73007300),('Veronica',77007700),('Jorge',22002200)]
-total_rows = len(lst)
-total_columns = len(lst[0])
+lst = [(1,'Ricardo Valladares',73007300),(2,'Veronica Arias',77007700),(3,'Jorge Carcamo',22002200)]
 
 class App(Tk):
-	def __init__(self):
-		super().__init__()
-		self.title("MySQL/MariaDB")
-		self.resizable(False, False)
-		self.mi_Label = Label(self, text="Nombre: ")
-		self.mi_Label.grid(row=0, column=0)
-		self.mi_Entry = Entry(self) 
-		self.mi_Entry.grid(row=0, column=1)
-		self.mi_Label1 = Label(self, text="Telefono: ")
-		self.mi_Label1.grid(row=1, column=0)
-		self.mi_Entry1 = Entry(self) 
-		self.mi_Entry1.grid(row=1, column=1)
-		self.boton1 = Button(self, text="Actualizar")
-		self.boton1['command'] = self.clicado
-		self.boton1.grid(row=2, column=0)
-		self.boton2 = Button(self, text="Insertar")
-		self.boton2.grid(row=2, column=1)
-		for i in range(total_rows):
-			for j in range(total_columns):
-				mi_Entry = Entry(self, width=20)
-				mi_Entry.grid(row=i+3, column=j)
-				mi_Entry.insert(END, lst[i][j])
-				mi_Entrys.append(mi_Entry)
-	def clicado(self):
-	    for btn in mi_Entrys:
-	        btn.destroy()		
-
+    def __init__(self):
+        super().__init__()
+        self.title("MySQL/MariaDB")
+        self.resizable(False, False)
+        self.label1 = Label(self, text="Nombre: ")
+        self.label1.grid(row=0, column=0, padx=10,pady=10)
+        self.textfield1 = Entry(self) 
+        self.textfield1.grid(row=0, column=1, padx=10,pady=10)
+        self.label2 = Label(self, text="Telefono: ")
+        self.label2.grid(row=1, column=0)
+        self.textfield2 = Entry(self) 
+        self.textfield2.grid(row=1, column=1)
+        self.boton1 = Button(self, text="Actualizar")
+        self.boton1['command'] = self.OnClick1
+        self.boton1.grid(row=2, column=0, padx=10,pady=10)
+        self.boton2 = Button(self, text="Insertar")
+        self.boton2['command'] = self.OnClick2
+        self.boton2.grid(row=2, column=1, padx=10,pady=10)
+        self.tabla = ttk.Treeview(self, selectmode ='browse')
+        self.tabla.grid(row=3,column=0, padx=10,pady=10, columnspan=2)
+        self.tabla['show'] = 'headings' 
+        self.columnas=['Id','Nombres','Telefonos']
+        self.tabla["columns"] = self.columnas
+        for i in self.columnas:
+            self.tabla.column(i, width = 100)
+        for i in self.columnas:
+            self.tabla.heading(i, text =i)
+        self.escrol = ttk.Scrollbar(self,orient="vertical", command=self.tabla.yview)#V Scrollbar
+        self.tabla.configure(yscrollcommand=self.escrol.set)  # connect to Treeview
+        self.escrol.grid(row=3,column=2,sticky='ns')
+        self.boton3 = Button(self, text="eliminar")
+        self.boton3['command'] = self.OnClick3
+        self.boton3.grid(row=4, column=1, padx=10,pady=10)
+        for i in range(len(lst)):
+            self.tabla.insert("",'end',iid=lst[i][0],text=lst[i][0],values=(lst[i][0],lst[i][1],lst[i][2]))
+    def OnClick1(self):
+        for row in self.tabla.get_children():
+            self.tabla.delete(row)
+    def OnClick2(self):
+        self.tabla.insert("",'end',iid=len(self.tabla.get_children())+1,text=len(self.tabla.get_children())+1,values=(len(self.tabla.get_children())+1,self.textfield1.get(),self.textfield2.get()) )       
+    def OnClick3(self):
+        item = self.tabla.item(self.tabla.focus())
+        print(item['text'])#values
+        
 
 if __name__ == "__main__":
-	app = App()
-	app.mainloop()
-
+    app = App()
+    app.mainloop()
